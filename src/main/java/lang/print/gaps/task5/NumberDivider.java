eb8d8926828ed6012bd0db88531eb65b29a18d7d
Apoffis <abc22_08@mail.ru>
2022-10-20 16:21:38 +0400

    task 5 commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import com.sun.jdi.Type;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double num = 5;
+        System.out.println(toBeDivided / num);
     }
 }
